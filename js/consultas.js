f7dce154d53449ba1bd853dc8a71b1f2b8481d06
Andrea <andreavmarin@gmail.com>
2020-12-14 00:30:21 -0300

    cambios de mensaje

--- js/consultas.js
+++ js/consultas.js
@@ -4,12 +4,12 @@ async function consultar(){
     console.log('DNI: ', dni);
     console.log('SOCIO: ', socio);
     
-    if(dni.length >= 6){
-        let peticion = await fetch(`http://localhost/apielecciones/Socio/DNI/${dni}`);
+    if(dni !== '' && dni.length >= 6){
+        let peticion = await fetch(`http://api.racingsiempre.com.ar/Socio/DNI/${dni}`);
         let respuesta = await peticion.json();
         console.log('Respuesta: ', respuesta);
     } else {
-        let peticion = await fetch(`http://localhost/apielecciones/Socio/S/${socio}`);
+        let peticion = await fetch(`http://api.racingsiempre.com.ar/Socio/S/${socio}`);
         let respuesta = await peticion.json();
         console.log('Respuesta: ', respuesta);
     }
